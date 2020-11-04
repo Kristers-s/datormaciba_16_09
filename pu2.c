e37c4e2bfeea4aae12115a77fb48826669ffdc08
kristers <kristers.sabansks@gmail.com>
2020-11-04 13:05:07 +0200

    pu2.c

--- pu2.c
+++ pu2.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 int main() {
-char a = 5;
-char b = 2;
-char  c = a << b;
-printf(" %d << %d = %d;\n",a,b,c);
+char  a = 0;
+printf("Ievadiet decimalskaitli ");
+scanf("%s",&a);
+a = a<<7;
+a = a>>7;
+a = a<<6;
+a = a>>7;
+a = a<<5;
+a = a>>7;
+a = a<<4;
+a = a>>7;
+a = a<<3;
+a = a>>7;
+a = a<<2;
+a = a>>7;
+a = a<<1;
+a = a>>7;
+printf("bin kods %d ",a);
 
 
 return 0;
